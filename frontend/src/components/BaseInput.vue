9d4f3caa02876fac991a0d5c7a2b2cc63603f661
JavaGath <i.izrailtenko@web.de>
2022-05-08 20:05:50 +0200

    Frontend: Modularized Components for Input

---
+++ frontend/src/components/BaseInput.vue
@@ -0,0 +1,62 @@
+<template>
+  <label :for="newID"> {{ label }}</label>
+  <br />
+  <input :type="inputType" :value="inputValue" />
+</template>
+
+<script>
+import NewID from '@/util/NewID'
+
+export default {
+  name: 'BaseInput',
+  props: {
+    label: {
+      type: String,
+      default: ''
+    },
+    inputType: {
+      type: String,
+      required: true
+    },
+    inputValue: {
+      type: [String, Number],
+      default: ''
+    }
+  },
+  setup() {
+    const newID = NewID().getID()
+
+    return { newID }
+  }
+}
+</script>
+
+<style scoped>
+label,
+input {
+  display: block;
+}
+
+label {
+  font-size: 10px;
+  float: left;
+}
+
+input {
+  background-color: white;
+  background-position: 10px 10px;
+  background-repeat: no-repeat;
+  padding-left: 6px;
+  margin-bottom: 15px;
+  margin-top: 5px;
+  height: 20px;
+  font-size: 18px;
+}
+
+input[type='submit'] {
+  display: inline;
+  height: 30px;
+  float: none;
+  background-color: #e476fa;
+}
+</style>
--- frontend/src/components/SignUpForm.vue
+++ frontend/src/components/SignUpForm.vue
@@ -2,54 +2,25 @@
   <div class="form-center">
     <form>
       <h1>¯\_(ツ)_/¯</h1>
-      <label>Username</label>
-      <br />
-      <input type="text" />
-      <label>Email address</label>
-      <br />
-      <input type="email" />
-      <label>Password</label>
-      <br />
-      <input type="password" />
-      <label>Repeat your password</label>
-      <br />
-      <input type="password" />
-      <input type="submit" value="Leak Data (づ｡◕‿‿◕｡)づ" />
+      <BaseInput label="Username" input-type="text"></BaseInput>
+      <BaseInput label="Email address" input-type="email"></BaseInput>
+      <BaseInput label="Password" input-type="password"></BaseInput>
+      <BaseInput label="Repeat your password" input-type="password"></BaseInput>
+      <BaseInput
+        input-type="submit"
+        input-value="Leak Data (づ｡◕‿‿◕｡)づ"
+      ></BaseInput>
     </form>
   </div>
 </template>
 
 <script>
+import BaseInput from '@/components/BaseInput'
+
 export default {
-  name: 'RegistryForm'
+  name: 'RegistryForm',
+  components: {
+    BaseInput
+  }
 }
 </script>
-
-<style scoped>
-label,
-input {
-  display: flex;
-}
-
-label {
-  font-size: 10px;
-  float: left;
-}
-
-input {
-  background-color: white;
-  background-position: 10px 10px;
-  background-repeat: no-repeat;
-  padding-left: 6px;
-  margin-bottom: 15px;
-  height: 20px;
-  font-size: 18px;
-}
-
-input[type='submit'] {
-  display: inline;
-  height: 30px;
-  float: none;
-  background-color: #e476fa;
-}
-</style>
