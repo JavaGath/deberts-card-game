db99d4551ad41c94a8106b512eb309642c8ddc6c
JavaGath <i.izrailtenko@web.de>
2022-05-13 20:19:29 +0200

    Backend: Controller and DTO for SignUp and User Data

--- backend/src/main/java/de/javagath/backend/web/DebertsController.java
+++ backend/src/main/java/de/javagath/backend/web/DebertsController.java
@@ -1,17 +1,25 @@
 package de.javagath.backend.web;
 
+import de.javagath.backend.web.model.SignUpDto;
+import java.lang.invoke.MethodHandles;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(maxAge = 3600)
 @RestController
-@RequestMapping("/")
+@RequestMapping("/user")
 public class DebertsController {
-  @GetMapping("/test")
-  public String getTest() {
 
-    return "HelloWorld";
+  private static final Logger LOG =
+      LoggerFactory.getLogger(MethodHandles.lookup().lookupClass().getSimpleName());
+
+  @PostMapping(value = "/signup", consumes = "application/json")
+  public void signUp(@RequestBody SignUpDto singUpDto) {
+    LOG.info(singUpDto.toString());
   }
 }
---
+++ backend/src/main/java/de/javagath/backend/web/model/SignUpDto.java
@@ -0,0 +1,75 @@
+package de.javagath.backend.web.model;
+
+public class SignUpDto {
+
+  private String name;
+  private String email;
+  private String password;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = getName().hashCode();
+    result = 31 * result + getEmail().hashCode();
+    result = 31 * result + getPassword().hashCode();
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof SignUpDto)) {
+      return false;
+    }
+
+    SignUpDto signUpDto = (SignUpDto) o;
+
+    if (!getName().equals(signUpDto.getName())) {
+      return false;
+    }
+    if (!getEmail().equals(signUpDto.getEmail())) {
+      return false;
+    }
+    return getPassword().equals(signUpDto.getPassword());
+  }
+
+  @Override
+  public String toString() {
+    return "SignUpDto{"
+        + "name='"
+        + name
+        + '\''
+        + ", email='"
+        + email
+        + '\''
+        + ", password='"
+        + password
+        + '\''
+        + '}';
+  }
+}
