ff8e15b4a98ddc401ef9232489baa9ac35574eba
JavaGath <i.izrailtenko@web.de>
2022-05-31 21:48:55 +0200

    Backend: Clean code for HistoryModeFilter.java

--- backend/src/main/java/de/javagath/backend/config/HistoryModeFilter.java
+++ backend/src/main/java/de/javagath/backend/config/HistoryModeFilter.java
@@ -13,26 +13,34 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+/**
+ * Enables HTML5 history mode for the embedded tomcat server. Single page applications use history
+ * mode to connect to other addresses. Servers like Node.js supports as default history mode for
+ * JS-Sources but tomcat does not. This filter implements this functionality for tomcat.
+ *
+ * @author Ievgenii Izrailtenko
+ * @version 1.0
+ * @since 1.0
+ */
 @Component
 public class HistoryModeFilter extends OncePerRequestFilter {
 
   private static final Logger LOG =
       LoggerFactory.getLogger(MethodHandles.lookup().lookupClass().getSimpleName());
-  // Update the regex as you needed
-  private Pattern patt = Pattern.compile("^/([^\\.])*");
-  private String endpoint = "/";
+  private final Pattern pattern = Pattern.compile("^/([^.])*");
 
   @Override
   protected void doFilterInternal(
       HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
       throws ServletException, IOException {
-    LOG.info(request.getRequestURI());
-    if (this.patt.matcher(request.getRequestURI()).matches()) {
-      LOG.info("I am in IF");
+    LOG.debug(request.getRequestURI());
+    if (pattern.matcher(request.getRequestURI()).matches()) {
+      LOG.debug("URI is matched");
+      String endpoint = "/";
       RequestDispatcher rd = request.getRequestDispatcher(endpoint);
       rd.forward(request, response);
     } else {
-      LOG.info("I am in ELSE");
+      LOG.debug("URI is not matched");
       filterChain.doFilter(request, response);
     }
   }
