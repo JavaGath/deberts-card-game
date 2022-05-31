6c937f76ff7c9adedb157ab6d2ec54152211c3b8
JavaGath <i.izrailtenko@web.de>
2022-05-31 21:09:27 +0200

    Backend: Clean code for WebSecurity.java

--- backend/src/main/java/de/javagath/backend/config/WebSecurity.java
+++ backend/src/main/java/de/javagath/backend/config/WebSecurity.java
@@ -2,7 +2,6 @@ package de.javagath.backend.config;
 
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,46 +10,58 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@Configuration
+/**
+ * Configuration Bean for Web Security. It enables CORS to act with Vue-Frontend and supports
+ * JWT-authentication in the application. Additionally, it uses the HistoryModeFilter to support Vue
+ * HTML5 History Mode.
+ *
+ * @author Ievgenii Izrailtenko
+ * @version 1.0
+ * @since 1.0
+ */
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true, jsr250Enabled = true, prePostEnabled = true)
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
-  @Autowired private JwtTokenFilter jwtTokenFilter;
+  private final JwtTokenFilter jwtTokenFilter;
+  private final HistoryModeFilter historyModeFilter;
 
-  @Autowired private HistoryModeFilter historyModeFilter;
+  /**
+   * DI of new implementation of WebSecurityConfigurerAdapter to manage HttpSecurity of the
+   * application.
+   *
+   * @param jwtTokenFilter DI to authenticate via JWT
+   * @param historyModeFilter DI to support Vue HTML5 History Mode
+   */
+  @Autowired
+  public WebSecurity(JwtTokenFilter jwtTokenFilter, HistoryModeFilter historyModeFilter) {
+    this.jwtTokenFilter = jwtTokenFilter;
+    this.historyModeFilter = historyModeFilter;
+  }
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    // Enable CORS and disable CSRF. CORS will protect the app from CSRF attacks.
     http.cors()
         .and()
         .csrf()
         .disable()
         .httpBasic()
         .disable()
-        // Set stateless session-management
         .sessionManagement()
         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        // Set unauthorized requests exception handler
         .and()
         .exceptionHandling()
         .authenticationEntryPoint(
-            (request, response, authException) -> {
-              response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
-            })
-        // Set permissions on endpoints
+            (request, response, authException) ->
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage()))
         .and()
         .authorizeRequests()
-        // TODO: Change after JWT and Vue router fix
         .antMatchers("/img/**", "/css/**", "/js/**", "/user/signup/", "/sign-up")
-        // .antMatchers("/**")
         .permitAll()
         .anyRequest()
         .authenticated()
         .and()
         .addFilterAfter(historyModeFilter, FilterSecurityInterceptor.class)
-        // Add JwtTokenFilter
         .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
   }
 }
