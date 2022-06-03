40f9fb88c243d1a3f4d9553b4d1f2413ab937341
JavaGath <i.izrailtenko@web.de>
2022-06-03 22:59:04 +0200

    Backend: Refactoring UserService.java

--- backend/src/main/java/de/javagath/backend/db/service/UserService.java
+++ backend/src/main/java/de/javagath/backend/db/service/UserService.java
@@ -14,47 +14,70 @@ import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+/**
+ * Utility class to handle operations with user and hist entity.
+ *
+ * @author Ievgenii Izrailtenko
+ * @version 1.0
+ * @since 1.0
+ */
 @Service
-public class UserUtil {
+public class UserService {
 
-  static final String bcryptInfo = "$2a$10$";
+  private static final String BCRYPT_INFO = "$2a$10$";
   private static final String PEPPER = "G8pfjFp34fLBew1eg5k";
+  private static final String EMAIL_PARAMETER_KEY = "email";
   private static final Logger LOG =
       LoggerFactory.getLogger(MethodHandles.lookup().lookupClass().getSimpleName());
   private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-  private SessionFactory hibernateFactory;
+  private final SessionFactory hibernateFactory;
 
+  /**
+   * Constructor for Service entity. EntityManagerFactory is needed to allow some Hibernate
+   * operations with UserEntity.
+   *
+   * @param factory Hibernate Entity Manager Factory
+   */
   @Autowired
-  public UserUtil(EntityManagerFactory factory) {
+  public UserService(EntityManagerFactory factory) {
     if (factory.unwrap(SessionFactory.class) == null) {
       throw new NullPointerException("factory is not a hibernate factory");
     }
     this.hibernateFactory = factory.unwrap(SessionFactory.class);
   }
 
+  /**
+   * Registries new user in the database from sign-up data.
+   *
+   * @param signUpDto sign-up data
+   */
   public void registry(SignUpDto signUpDto) {
-
     UserEntity newUser = createUserEntity(signUpDto);
-
     Session session = hibernateFactory.openSession();
-    LOG.info("Test Save Begin!");
+    LOG.debug("Try to save user");
     session.save(newUser);
-    LOG.info("Test Save End!");
+    LOG.debug("User is saved");
   }
 
+  /**
+   * Selects user using his email address.
+   *
+   * @param email users email address.
+   * @return selected users entity
+   */
   public UserEntity selectUserByEmail(String email) {
     Session session = hibernateFactory.openSession();
     Query query = session.createQuery("from UserEntity u where u.email = :email");
-    query.setParameter("email", email);
+    query.setParameter(EMAIL_PARAMETER_KEY, email);
     return (UserEntity) query.getSingleResult();
   }
 
   private String encodePassword(String password, String salt) {
     LOG.debug("Plain Password: " + password);
-    Integer passwordValue = password.hashCode() + PEPPER.hashCode();
-    String passwordBcrypt = BCrypt.hashpw(passwordValue.toString(), salt);
+    int passwordValue = password.hashCode() + PEPPER.hashCode();
+    String passwordBcrypt = BCrypt.hashpw(Integer.toString(passwordValue), salt);
     String passwordEncoded = passwordEncoder.encode(passwordBcrypt);
-    String passwordReady = passwordEncoded.substring(bcryptInfo.length());
+    String passwordReady = passwordEncoded.substring(BCRYPT_INFO.length());
     LOG.debug("Password to Store: " + passwordReady);
     return passwordReady;
   }
@@ -64,7 +87,7 @@ public class UserUtil {
     newUser.setName(signUpDto.getUsername());
     newUser.setEmail(signUpDto.getEmail());
     String salt = BCrypt.gensalt();
-    newUser.setSalt(salt.substring(bcryptInfo.length()));
+    newUser.setSalt(salt.substring(BCRYPT_INFO.length()));
     String password = encodePassword(signUpDto.getPassword(), salt);
     newUser.setPassword(password);
     return newUser;
